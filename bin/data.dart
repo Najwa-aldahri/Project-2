350fb2951db139c2846457ab531e5410dafeb96a
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-08-09 13:51:27 +0300

    done with the json

---
+++ bin/data.dart
@@ -0,0 +1,49 @@
+Map <String,dynamic>dataSet = {
+  "library": [
+    {
+      "id": "1",
+      "title": "The Great Gatsby",
+      "authors": ["F. Scott Fitzgerald"],
+      "categories": ["Fiction"],
+      "year": 1925,
+      "quantity": 5,
+      "price": 10.99
+    },
+    {
+      "id": "2",
+      "title": "To Kill a Mockingbird",
+      "authors": ["Harper Lee"],
+      "categories": ["Fiction", "Classic"],
+      "year": 1960,
+      "quantity": 0,
+      "price": 12.99
+    },
+    {
+      "id": "3",
+      "title": "1984",
+      "authors": ["George Orwell"],
+      "categories": ["Dystopian", "Science Fiction"],
+      "year": 1949,
+      "quantity": 3,
+      "price": 15.49
+    },
+    {
+      "id": "4",
+      "title": "Pride and Prejudice",
+      "authors": ["Jane Austen"],
+      "categories": ["Romance", "Classic"],
+      "year": 1813,
+      "quantity": 4,
+      "price": 8.99
+    },
+    {
+      "id": "5",
+      "title": "The Catcher in the Rye",
+      "authors": ["J.D. Salinger"],
+      "categories": ["Fiction"],
+      "year": 1951,
+      "quantity": 2,
+      "price": 13.49
+    }
+  ]
+};
